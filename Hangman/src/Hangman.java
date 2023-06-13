46b2da0afb25338a393a0d74d0041813005513fb
Jinho Nam <nam.6@wright.edu>
2023-06-13 14:46:59 -0400

    extracted a word from text file randomly

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -1,12 +1,42 @@
 package Hangman.src;
-// TODO1: Read wordslist
-// TODO2: 
-// TODO2: JButton (We need this w a separate class)
+
+// TODO2: choose a random word in the arraylist and display it with underscores
+// make a label as many as the number of letters in the word ?
+// TODO2-1: there will be 'n' underscores as proportion to the number of letters in the chosen word.
+// TODO2-2: make a class named "word"
+
+// TODO3: JButton (We need this w a separate class)
 import javax.swing.*;
 import java.awt.*;
 import java.io.File; // to find a path of ImageIcon named "icon"
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Hangman {
+    // RightTopPanel
+    // choose a random word from the word list 
+    public static String getRandomWord(ArrayList<String> wordList) {
+        Random randNumGen = new Random();
+        int randNum = randNumGen.nextInt(wordList.size()) + 1;
+        String randomWord = wordList.get(randNum);
+        return randomWord;
+    }
+
+    // RightTopPanel
+    // read contents of the file and make an arraylist of them
+    public static ArrayList<String> extractTextFile() throws FileNotFoundException {
+        ArrayList<String> wordList = new ArrayList<>();
+        Scanner readFile = new Scanner(new File("/home/jinhoishere/github/projects/Hangman/lib/wordList.txt"));
+        while (readFile.hasNextLine()) {
+            String currentWord = readFile.nextLine();
+            wordList.add(currentWord);
+        }
+        return wordList;
+    }
+
+    // a method to adjust the size of images comfortably.
     public static Icon resizeIcon(ImageIcon icon, int resizedWidth, int resizedHeight) {
         Image img = icon.getImage();
         Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight, 0);
@@ -52,7 +82,7 @@ public class Hangman {
 
 
         // (JFrame)frame = (JPanel)mainPanel
-        JFrame frame = new JFrame("Hangman by Jinho Nam");
+        JFrame frame = new JFrame("Hangman");
         ImageIcon icon = new ImageIcon("/home/jinhoishere/github/projects/Hangman/lib/H.png");
         // System.out.println(new File("/home/jinhoishere/github/projects/Hangman/lib/H.png").exists()); 
         // ImageIcon icon = new ImageIcon("/home/jinhoishere/projects/Hangman/lib/H.png");
@@ -66,9 +96,16 @@ public class Hangman {
 
         frame.add(mainPanel);
         frame.pack();
-      
     }
-    public static void main (String[] args) {
+
+    public static void main (String[] args) throws FileNotFoundException{
+        System.out.println("----------------------------");
+        System.out.println("| <Hangman by Jinho Nam>   |");
+        System.out.println("|- Github: jinhoishere     |");
+        System.out.println("|- Discord: Caffeine#3490  |");
+        System.out.println("|- Email: nam.6@wright.edu |");
+        System.out.println("----------------------------");
         new Hangman();
+        
     }
 }
