dd55ca8fb45c169669324c9f8a8bc485e9885f0b
Jinho <nam.6@wright.edu>
2023-08-19 18:40:20 -0400

    a class to treat pages as objects

--- ListLinks/src/Page.java
+++ ListLinks/src/Page.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import org.jsoup.Jsoup;
@@ -9,8 +11,8 @@ import org.jsoup.select.Elements;
 public class Page {
     // fields
     private String url; // a URL of the current page
-    private Elements links; // extracted 50 links on the current page
-    private String randLink; // a random link among 50 links on the current page
+    private ArrayList<String> links; // extracted 30 links from "this.url"
+    private String pathLink; // a randomly picked link among "links" to create a url of next page
 
     // default constructor
     public Page() {
@@ -27,31 +29,51 @@ public class Page {
     }
 
     // extract links on a page of the given URL
-    public void setLinksFrom(String url) throws IOException {
+    public void extractLinksFrom(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
-        Elements origianl_links = doc.select("a[href]");
-        // only picks first 50 links from the top
-        for (int i = 0; i < 50; i++) {
-            Element new_link = origianl_links.get(i);
-            String absHref = new_link.attr("abs:href");
+        Elements original_links = doc.select("a[href]");
+        ArrayList<String> abs_links = new ArrayList<>();
+        // pick first 30 links from the top
+        // if the page has less than 30, pick all links
+        if (original_links.size() < 30) {
+            for (int i = 0; i < original_links.size(); i++) {
+                Element currentLink = original_links.get(i);
+                String absHref = currentLink.attr("abs:href");
+                abs_links.add(absHref);
+            }
         }
-        this.links = origianl_links;
+        else {
+            for (int i = 0; i < 30; i++) {
+                Element currentLink = original_links.get(i);
+                String absHref = currentLink.attr("abs:href");
+                abs_links.add(absHref);
+            }
+        }
+        this.links = trimmed(abs_links);
+        Collections.shuffle(this.links);
     }
 
-    public Elements getLinks() {
+    public ArrayList<String> getLinks() {
         return this.links;
     }
 
-    // pick a random link among links on that page
-    public void setRandomLinkAmong(Elements links) {
+    // pick a random link among 30 links on that page
+    public void setPathLink(ArrayList<String> links) {
         Random randGen = new Random();
-        int randNum = randGen.nextInt(links.size()) + 1;
-        Element link = links.get(randNum);
-        String absHref = link.attr("abs:href");
-        this.randLink = absHref;
+        int randNum = randGen.nextInt(links.size());
+        String randLink = links.get(randNum);
+        this.pathLink = randLink;
     }
 
-    public String getRandomLink() {
-        return this.randLink;
+    public String getPathLink() {
+        return this.pathLink;
+    }
+
+    // a method to remove "#bodyContent" from the first link
+    public ArrayList<String> trimmed(ArrayList<String> absLinks) {
+        String s = absLinks.get(0);
+        String modified_s = s.substring(0, s.length()-12);
+        absLinks.set(0, modified_s);
+        return absLinks;
     }
 }
